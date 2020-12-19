a2d3f3372dcea1c9e4cee4464f4335aa6a0653ea
TimurDian <timur7dian84@gmail.com>
2020-12-19 16:05:47 +0700

    Add Service Midtrans User Transaction API

--- handler/transaction.go
+++ handler/transaction.go
@@ -47,10 +47,39 @@ func (h *transactionHandler) GetUserTransaction(c *gin.Context) {
 
 	transactions, err := h.transactionService.GetTransactionByUserID(userID)
 	if err != nil {
-		response := helper.APIResponse("Failed to get user's of transactionsa", http.StatusBadRequest, "error", nil)
+		response := helper.APIResponse("Failed to get user's of transactions", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 	response := helper.APIResponse("User's transactions", http.StatusOK, "success", transaction.FormatUserTransactions(transactions))
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *transactionHandler) CreateTransaction(c *gin.Context) {
+	var input transaction.CreateTransactionInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidatorError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to create transaction", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+
+	newTransaction, err := h.transactionService.CreateTransaction(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create transaction", http.StatusOK, "error", transaction.FormatTransaction(newTransaction))
+	c.JSON(http.StatusOK, response)
+	return
+
 }
